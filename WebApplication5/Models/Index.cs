9cd0d9438ed198d8ff927ba3d7b910e99cbce191
Samantha Troy <S.troy1220@gmail.com>
2017-12-08 16:42:57 -0500

    form submission  to google sheet

---
+++ WebApplication5/Models/Index.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+//namespace WebApplication5.Models
+//{
+//    public class IndexModel
+//    {
+//        [Display(Name = "First Name")]
+//        public string FirstName { get; set; }
+//        [Display(Name = "Last Name")]
+//        public string LastName { get; set; }
+//        [Display(Name = "Email")]
+//        public string EmailAddresss { get; set; }
+//        [Display(Name = "Street Address")]
+//        public string StreetAddress { get; set; }
+//        public string City { get; set; }
+//        public string State { get; set; }
+//        public int Zip { get; set; }
+//        public string Phone { get; set; }
+//    }
+//}
